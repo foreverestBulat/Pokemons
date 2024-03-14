a0fc1b8c04ed0d7b61cbc700ec29f3af0bc22edb
foreverestBulat <113981993+foreverestBulat@users.noreply.github.com>
2024-03-14 20:30:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="" exact element={<Pokemons/>}/>
+      <Route path="/" exact element={<Pokemons/>}/>
         <Route path="/my-app" exact element={<Pokemons/>}/>
         <Route path="/pokemons" element={<Pokemons/>} />
         <Route path="/pokemons/:id" element={<Pokemon/>} />
@@ -16,23 +18,3 @@ function App() {
 }
 
 export default App;
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './App.css';
-// import Main from './components/main';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import Pokemons from './pages/pokemons';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Route path="/pokemon">
-//         <Pokemons/>  
-//       </Route>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
